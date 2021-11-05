df492fcc9b766a4a9172e2b927c9634088e58873
Motiff <lqcyt20010122@gmail.com>
2021-11-05 22:53:32 +0800

    new version

--- curvetest.js
+++ curvetest.js
@@ -13,9 +13,26 @@ function preload() {
     //myFont = loadFont('assets/fzdys.ttf');
 }
 
+function colorMap(a){
+    var re;
+    switch(a){
+        case 0:
+            re = createVector(0,0,100);
+            break;
+        case 1:
+            re = createVector(205,100,100);
+            break;
+        case 2:
+            re = createVector(236,100,100);
+        break;
+    }
+    return re;
+}
+
 function setup() {
     var cvs = createCanvas(windowWidth, windowHeight);
     cvs.mouseReleased(detect);
+    frameRate(30);
     noStroke();
     colorMode(HSB,360,100,100,1);
     ellipseMode(CENTER);
@@ -23,7 +40,7 @@ function setup() {
     background(0,0,100);
     inputArray = new myPoints();
     inputWord = new myWordConsole(fstword);
-    theNoise = new perlinController(1000,2);
+    theNoise = new perlinController(50,2);
     theNoise.generate();
 }
 
@@ -84,9 +101,12 @@ class myWordConsole {
     constructor(word,theArray) {
         this.array = theArray;
         this.word = word;
+        this.wordPos = 0;
         this.maxLength = 5;
-        this.lowDis = 20;
+        this.lowDis = 25;
         this.list = [];
+        this.currentPos = 0;
+        this.color = colorMap(int(random(3)));
         this.reset = function (theArray) {
             this.array = theArray;
             this.list = [];
@@ -105,36 +125,34 @@ class myWordConsole {
                 //var singleDis = lineLen / (this.word.length + 2);
 
                 var currentDis = 0;
-                var currentVertex = 0;
-
-                for (var i = 0; i < this.maxLength-1; i++) {
+                if(this.list.length<this.maxLength-1){
                     currentDis += singleDis;
-                    //print(currentVertex);
-                    if (currentDis < this.array.list[currentVertex].dis) {
+                    //print(this.currentPos);
+                    if (currentDis < this.array.list[this.currentPos].dis) {
 
                     } else {
-                        while(currentDis >= this.array.list[currentVertex].dis && currentVertex < this.array.list.length-1) {
-                            var nowDis = this.array.list[currentVertex].dis;
+                        while(currentDis >= this.array.list[this.currentPos].dis && this.currentPos < this.array.list.length-1) {
+                            var nowDis = this.array.list[this.currentPos].dis;
                             currentDis -= nowDis;
-                            currentVertex += 1;
+                            this.currentPos += 1;
                         }
                     }
-                    var solution = KandB(this.array.list[currentVertex].x, this.array.list[currentVertex].y, this.array.list[currentVertex + 1].x, this.array.list[currentVertex + 1].y);
-                    var rate = currentDis / this.array.list[currentVertex].dis;
-                    var x = (this.array.list[currentVertex + 1].x - this.array.list[currentVertex].x) * rate + this.array.list[currentVertex].x;
-                    if(this.array.list[currentVertex + 1].x - this.array.list[currentVertex].x === 0){
-                        var y = (this.array.list[currentVertex+1].y - this.array.list[currentVertex].y)* rate + this.array.list[currentVertex].y;
+                    var solution = KandB(this.array.list[this.currentPos].x, this.array.list[this.currentPos].y, this.array.list[this.currentPos + 1].x, this.array.list[this.currentPos + 1].y);
+                    var rate = currentDis / this.array.list[this.currentPos].dis;
+                    var x = (this.array.list[this.currentPos + 1].x - this.array.list[this.currentPos].x) * rate + this.array.list[this.currentPos].x;
+                    if(this.array.list[this.currentPos + 1].x - this.array.list[this.currentPos].x === 0){
+                        var y = (this.array.list[this.currentPos+1].y - this.array.list[this.currentPos].y)* rate + this.array.list[this.currentPos].y;
                     }
                     else {
                         var y = solution.x * x + solution.y;
                     }
-                    if(this.list[i] !== new myWord(x, y, tmpSize, this.word[i], solution.x,1))
-                        this.list[i] = new myWord(x, y, tmpSize, this.word[i], solution.x,1);
+                    this.list[this.list.length] = new myWord(x, y, tmpSize, this.word[this.wordPos], solution.x,1,this.color);
+                    this.wordPos += 1;
                 }
             }
         }
         this.draw = function () {
-            for(var i = 0; i < this.list.length; i++) {
+            for(let i = 0; i < this.list.length; i++) {
                 if(this.list[i].drew === false)
                     this.list[i].draw();
                 //print("drawn");
@@ -144,7 +162,7 @@ class myWordConsole {
 }
 
 class myWord{
-    constructor(x,y,s,str,k,dir) {
+    constructor(x,y,s,str,k,dir,inColor) {
         this.drew = false;
         this.x = x;
         this.y = y;
@@ -153,13 +171,13 @@ class myWord{
         //决定字的朝向
         this.ang = abs(atan2( k,1));
         this.dir = dir;
+        this.color = inColor;
         this.draw = function () {
-            this.drew = true;
             translate(this.x,this.y);
             rotate(this.dir*this.ang);
             textSize(this.size);
             //fill(random(360),100,100);
-            fill(360,0,0);
+            fill(this.color.x,this.color.y,this.color.z);
             text(this.word, 0, 0);
             rotate(-this.dir*this.ang);
             translate(-this.x,-this.y);
@@ -214,17 +232,17 @@ class noisePoint{
         this.console = new myWordConsole(fstword,this.array);
         this.life = int(random(500))+100;
         this.color = createVector(int(random(360)),100,100);
-        this.pos = createVector(int(random(windowWidth/10)*10),int(random(windowHeight/10)*10));
+        this.pos = createVector(int(random(windowWidth)),int(random(windowHeight)));
 
         this.update = function () {
             var arg = 0.0;
-            arg = noise(this.pos.x/windowWidth,this.pos.y/windowHeight) *2 *PI;
+            arg = noise(this.pos.x/this.scale,this.pos.y/this.scale) *8 *PI;
             this.pos.x += cos(arg)*this.step;
             this.pos.y += sin(arg)*this.step;
             this.life -= 1;
             this.array.updateByNoise(this.pos.x,this.pos.y);
             this.array.draw();
-            if(this.array.list.length>5){
+            if(this.array.list.length>1){
                 this.console.update();
                 this.console.draw();
             }
@@ -237,7 +255,7 @@ class noisePoint{
 
 //主程序
 function draw() {
-    background(360,0,100);
+    //background(360,0,100);
     /*
     if(doubvarouch){
         inputArray.clear();
